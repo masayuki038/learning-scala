9c15f0bb6561f76ecddeb4f7441c652c67f1e869
Masayuki Takahashi <masayuki038@gmail.com>
2018-05-05 12:49:58 +0900

    Add "10.13 Defining a factory object"

--- src/main/scala/net/wrap_trap/learning_scala/chapter10/Element.scala
+++ src/main/scala/net/wrap_trap/learning_scala/chapter10/Element.scala
@@ -1,8 +1,33 @@
 package net.wrap_trap.learning_scala.chapter10
 
+object Element {
+
+  private class ArrayElement(val contents: Array[String]) extends Element {}
+
+  private class LineElement(s: String) extends Element {
+    val contents = Array(s)
+    override def width = s.length
+    override def height = 1
+  }
+
+  private class UniformElement(ch: Char, override val width: Int, override val height: Int) extends Element {
+    private val line = ch.toString * width
+    def contents = for (_ <- (1 to height).toArray) yield line
+  }
+
+  def elem(contents: Array[String]): Element = new ArrayElement(contents)
+
+  def elem(chr: Char, width: Int, height: Int): Element = new UniformElement(chr, width, height)
+
+  def elem(line: String): Element = new LineElement(line)
+}
+
 abstract class Element {
+
   def contents: Array[String]
+
   def height: Int = contents.length
+
   def width: Int = if (height == 0) 0 else contents(0).length
 
   def above(that: Element): ArrayElement = {
